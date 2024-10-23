a627dc92cc1492ff9842a27b5f26a152c8627795
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-10-23 17:39:34 +0700

    memberikan efek shadow pada sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -20,7 +20,8 @@ const MenuItem = ({ icon, label }) => {
 
 function Sidebar() {
   return (
-    <aside className="w-56 h-screen bg-[#FFB1B1] text-[#921A40] flex flex-col rounded-r-[2rem]">
+    <aside className="w-56 h-screen bg-[#FFB1B1] text-[#921A40] flex flex-col rounded-r-[2rem] shadow-[0_10px_20px_rgba(0,0,0,0.3)] 
+      before:content-[''] before:absolute before:inset-0 before:rounded-r-[2rem] before:shadow-[inset_0_4px_6px_rgba(0,0,0,0.2)] relative">
       <div className="px-4 py-2">
         {/* Logo */}
         <div className="flex items-center space-x-4">
@@ -43,7 +44,7 @@ function Sidebar() {
       </nav>
 
       {/* Button Exit */}
-      <button className="flex items-center space-x-6 py-3 px-6 mt-auto hover:bg-[#921A40] hover:bg-opacity-80 hover:text-white rounded-br-[2rem]">
+      <button className="flex items-center space-x-6 py-3 px-6 mt-auto hover:bg-[#921A40] hover:bg-opacity-80 hover:text-white rounded-br-[2rem] shadow-[0_10px_20px_rgba(0,0,0,0.3)]">
         <FiLogOut className="w-5 h-5 mx-2" />
         <span className="text-lg font-semibold">Kembali</span>
       </button>
