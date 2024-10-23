2d3d3e39dcda6ba9e093417c7a6186741bcfb1db
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-10-23 19:04:26 +0700

    update sidebar agar menu dapat menuju path yang ditentukan dan mengaktifkan hover saat berada di pagenya

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import logoWecare from '../assets/image/logoWecare.png';
 import { FiUser, FiLogOut } from 'react-icons/fi';
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
@@ -6,22 +7,23 @@ import { BiBriefcase } from "react-icons/bi";
 import { MdOutlineStorage } from "react-icons/md";
 import { LuFileSignature } from "react-icons/lu";
 
-const MenuItem = ({ icon, label }) => {
+const MenuItem = ({ icon, label, to, isActive }) => {
   return (
-    <a
-      href="#"
-      className="flex items-center space-x-8 py-3 px-6 hover:bg-[#921A40] hover:bg-opacity-80 hover:text-white"
+    <Link
+      to={to}
+      className={`flex items-center space-x-8 py-3 px-6 ${isActive ? 'bg-[#921A40] text-white' : 'hover:bg-[#921A40] hover:bg-opacity-80 hover:text-white'} transition duration-300 ease-in-out`}
     >
       {icon}
       <span className="text-lg font-semibold">{label}</span>
-    </a>
+    </Link>
   );
 };
 
 function Sidebar() {
+  const location = useLocation(); // Mengambil lokasi saat ini
+
   return (
-    <aside className="w-56 h-screen bg-[#FFB1B1] text-[#921A40] flex flex-col rounded-r-[2rem] shadow-[0_10px_20px_rgba(0,0,0,0.3)] 
-      before:content-[''] before:absolute before:inset-0 before:rounded-r-[2rem] before:shadow-[inset_0_4px_6px_rgba(0,0,0,0.2)] relative">
+    <aside className="w-56 h-screen bg-[#FFB1B1] text-[#921A40] flex flex-col rounded-r-[2rem] shadow-[0_10px_20px_rgba(0,0,0,0.3),inset_0_4px_6px_rgba(0,0,0,0.2)] relative">
       <div className="px-4 py-2">
         {/* Logo */}
         <div className="flex items-center space-x-4">
@@ -36,15 +38,15 @@ function Sidebar() {
 
       {/* Menu */}
       <nav className="flex-grow mt-10 space-y-4">
-        <MenuItem icon={<FiUser className="w-5 h-5 mx-1" />} label="Profile" />
-        <MenuItem icon={<MdOutlineStorage className="w-5 h-5 mx-1" />} label="Pelatihan" />
-        <MenuItem icon={<HiOutlineClipboardDocumentList className="w-5 h-5 mx-1" />} label="Portfolio" />
-        <MenuItem icon={<LuFileSignature className="w-5 h-5 mx-1" />} label="CV Generator" />
-        <MenuItem icon={<BiBriefcase className="w-5 h-5 mx-1" />} label="Lowongan" />
+        <MenuItem icon={<FiUser className="w-5 h-5 mx-1" />} label="Profile" to="/profile" isActive={location.pathname === '/profile'} />
+        <MenuItem icon={<MdOutlineStorage className="w-5 h-5 mx-1" />} label="Pelatihan" to="/pelatihan" isActive={location.pathname === '/pelatihan'} />
+        <MenuItem icon={<HiOutlineClipboardDocumentList className="w-5 h-5 mx-1" />} label="Portfolio" to="/portfolio" isActive={location.pathname === '/portfolio'} />
+        <MenuItem icon={<LuFileSignature className="w-5 h-5 mx-1" />} label="CV Generator" to="/cv-generator" isActive={location.pathname === '/cv-generator'} />
+        <MenuItem icon={<BiBriefcase className="w-5 h-5 mx-1" />} label="Lowongan" to="/lowongan" isActive={location.pathname === '/lowongan'} />
       </nav>
 
       {/* Button Exit */}
-      <button className="flex items-center space-x-6 py-3 px-6 mt-auto hover:bg-[#921A40] hover:bg-opacity-80 hover:text-white rounded-br-[2rem] shadow-[0_10px_20px_rgba(0,0,0,0.3)]">
+      <button className="flex items-center space-x-6 py-3 px-6 mt-auto hover:bg-[#921A40] hover:bg-opacity-80 hover:text-white transition duration-300 ease-in-out rounded-br-[2rem] shadow-[0_10px_20px_rgba(0,0,0,0.3)]">
         <FiLogOut className="w-5 h-5 mx-2" />
         <span className="text-lg font-semibold">Kembali</span>
       </button>
