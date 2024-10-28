6b25a205d5fc776e533086f5aa8d9f021103cd71
nataliaSiahaan <nataliatasyasiahaan@gmail.com>
2024-10-28 10:21:08 +0700

    mengubah pemanggilan router Dasboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import DashboardProfile from './pages/DashboardProfile';  
+import DashboardPelatihan from './pages/DashboardPelatihan'; 
 
 function App() {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-grow p-4">
-        <Outlet />
-      </div>
+    <div className="bg-background">
+      <Routes>
+        {/* Route ke Homepage dan page lainnya */}
+        {/* <Route path="/" element={<Homepage />} /> */}
+
+        <Route path="/dashboard" element={<Dashboard />}>
+          {/* Rute anak untuk Dashboard */}
+          <Route path="profile" element={<DashboardProfile />} />
+          <Route path="pelatihan" element={<DashboardPelatihan />} />
+          {/* Rute Dashboard lainnya */}
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,49 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { 
-    createBrowserRouter, 
-    RouterProvider 
-  } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-import App from './App.jsx';
-import './index.css';
-
-import DashboardProfile from './pages/DashboardProfile.jsx';
-import DashboardPelatihan from './pages/DashboardPelatihan.jsx';
-import DashboardCVGenerator from './pages/DashboardCVGenerator.jsx';
-import DashboardPortfolio from './pages/DashboardPortfolio.jsx';
-import DashboardLowongan from './pages/DashboardLowongan.jsx';
-
-// Mengatur routing
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <DashboardProfile />,
-  },
-  {
-    path: "pelatihan",
-    element: <DashboardPelatihan />,
-  },
-  {
-    path: "portfolio",
-    element: <DashboardPortfolio/>,
-  },
-  {
-    path: "cv-generator",
-    element: <DashboardCVGenerator />,
-  },
-  {
-    path: "lowongan",
-    element: <DashboardLowongan/>,
-  },
-]);
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
