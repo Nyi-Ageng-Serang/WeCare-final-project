fea7ed143ac3cb6229e7d7a272aac7068efaff8e
Komalasari076 <sitikomala076@gmail.com>
2024-10-30 16:35:30 +0700

    membuat component aside

--- src/components/Forum/Forum.jsx
+++ src/components/Forum/Forum.jsx
@@ -1,9 +1,11 @@
 import Hero from "./HeroSection";
+import Sidebar from "./Sidebar";
 
 function Forum() {
   return (
     <>
       <Hero/>
+      <Sidebar/>
     </>
   );
 }
---
+++ src/components/Forum/Sidebar.jsx
@@ -0,0 +1,33 @@
+import add from "../../assets/Add.png";
+
+function Sidebar() {
+  const topics = ["Freelance", "Tips & Trik", "Life Style", "STEM"];
+
+  return (
+    <>
+      <div className="w-1/4 my-10">
+        <button className="bg-softPink text-gray-800 font-semibold w-full py-2 mb-4 rounded-lg shadow-md">
+          <img src={add} alt="" className="inline-block mr-4" />
+          Posting Diskusi Baru
+        </button>
+
+        <div className="font-semibold text-red">
+          <p className="mt-10 my-4">Daftar Topik Diskusi</p>
+
+          <ul className="space-y-2">
+            {topics.map((topic) => (
+              <li
+                key={topic}
+                className="bg-softPink p-2 rounded text-pink-600 cursor-pointer"
+              >
+                {topic}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Sidebar;
