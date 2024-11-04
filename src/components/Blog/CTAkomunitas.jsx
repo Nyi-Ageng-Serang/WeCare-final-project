f00e881706b699075443c42e0c02a32a8ec8b4cc
Komalasari076 <sitikomala076@gmail.com>
2024-11-04 20:00:26 +0700

    update style blog

--- src/components/Blog/CTAkomunitas.jsx
+++ src/components/Blog/CTAkomunitas.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import komunitasIMG from '../../assets/komunitas.png';
+import React from "react";
+import komunitasIMG from "../../assets/komunitas.png";
 
 const CommunityCTA = () => {
   return (
-    <section className="bg-pink bg-opacity-20 flex items-center justify-center py-24">
-      <div className="flex flex-col md:flex-row items-center max-w-5xl mx-auto space-y-6 md:space-y-0 md:space-x-40">
-        
-        {/* Text Section */}
-        <div className="md:text-left space-y-8">
-          <h2 className="text-4xl font-bold text-[#921A40]">
-            Bergabunglah dengan komunitas WeCare
-          </h2>
-          <p className="text-gray text-lg pr-24">
-            Temukan Dukungan Sesama Perempuan yang Kembali Berkarier!
-          </p>
-          <button className="bg-[#921A40] text-white font-semibold py-2 px-4 rounded-lg hover:bg-[#922d69] transition">
-            Gabung Sekarang
-          </button>
-        </div>
+    <div className="w-full py-6 my-6">
+      <section className="bg-pink bg-opacity-20">
+        <div className="container mx-auto px-10 py-24">
+          <div className="flex flex-col md:flex-row items-center mx-auto max-w-5xl space-y-6 md:space-y-0 md:space-x-40">
+            {/* Text Section */}
+            <div className="md:text-left space-y-8 mb-20 md:mb-0">
+              <h2 className="text-4xl font-bold text-[#921A40]">
+                Bergabunglah dengan komunitas WeCare
+              </h2>
+              <p className="text-gray text-lg pr-24">
+                Temukan Dukungan Sesama Perempuan yang Kembali Berkarier!
+              </p>
+              <button className="bg-[#921A40] text-white font-semibold py-2 px-4 rounded-lg hover:bg-[#922d69] transition">
+                Gabung Sekarang
+              </button>
+            </div>
 
-        {/* Image Section */}
-        <div className="relative flex-shrink-0">
-            <div className="bg-[#921A40] w-48 h-[32rem] absolute top-1/2 transform -translate-y-1/2 -translate-x-1/2 z-0 rotate-45 rounded-[100%] left-1/2"></div>
-            <div className="bg-[#921A40] w-48 h-[32rem] absolute top-1/2 transform -translate-y-1/2 translate-x-1/2 z-0 rotate-[135deg] rounded-[100%] right-1/2"></div>
+            {/* Image Section */}
+            <div className="relative flex-shrink-0">
+              <div className="bg-[#921A40] w-40 h-[24rem] absolute top-1/2 transform -translate-y-1/2 -translate-x-1/2 z-0 rotate-45 rounded-[100%] left-1/2"></div>
+              <div className="bg-[#921A40] w-40 h-[24rem] absolute top-1/2 transform -translate-y-1/2 translate-x-1/2 z-0 rotate-[135deg] rounded-[100%] right-1/2"></div>
 
-            <img 
+              <img
                 src={komunitasIMG}
-                alt="Women smiling" 
-                className="relative z-10 rounded-full w-72 h-72 object-cover"
-            />
+                alt="Women smiling"
+                className="relative z-10 rounded-full w-64 h-64 object-cover"
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
--- src/components/Blog/FAQSection.jsx
+++ src/components/Blog/FAQSection.jsx
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
-import { AiOutlineHeart, AiOutlineUser, AiOutlineDatabase, AiOutlineRise } from 'react-icons/ai';
+import React, { useState } from "react";
+import {
+  AiOutlineHeart,
+  AiOutlineUser,
+  AiOutlineDatabase,
+  AiOutlineRise,
+} from "react-icons/ai";
 
 const FAQSection = () => {
   const [openIndex, setOpenIndex] = useState(null);
@@ -8,19 +13,41 @@ const FAQSection = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row justify-between py-24 px-32 bg-[#FFB1B1] bg-opacity-25 md:space-x-40">
+    <div className="flex flex-col md:flex-row justify-between py-24 lg:px-32 px-10 bg-[#FFB1B1] bg-opacity-25 md:space-x-40">
       {/* Stats Section */}
-      <div className="grid grid-cols-2 gap-8 mb-8 md:mb-0 text-center">
-        <StatCard icon={<AiOutlineHeart className="text-3xl text-[#921A40]" />} value="400+" label="Pengguna Senang"  className="translate-y-8 "  />
-        <StatCard icon={<AiOutlineDatabase className="text-3xl text-[#921A40]" />} value="200+" label="Pelatihan Terpercaya"/>
-        <StatCard icon={<AiOutlineUser className="text-3xl text-[#921A40]" />} value="30+" label="Pengguna Baru"  className="translate-y-8"/>
-        <StatCard icon={<AiOutlineRise className="text-3xl text-[#921A40]" />} value="100+" label="Cerita Sukses" />
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-8 mb-8 md:mb-0 text-center">
+        <StatCard
+          icon={<AiOutlineHeart className="text-3xl text-[#921A40]" />}
+          value="400+"
+          label="Pengguna Senang"
+          className="md:translate-y-8 "
+        />
+        <StatCard
+          icon={<AiOutlineDatabase className="text-3xl text-[#921A40]" />}
+          value="200+"
+          label="Pelatihan Terpercaya"
+        />
+        <StatCard
+          icon={<AiOutlineUser className="text-3xl text-[#921A40]" />}
+          value="30+"
+          label="Pengguna Baru"
+          className="md:translate-y-8 "
+        />
+        <StatCard
+          icon={<AiOutlineRise className="text-3xl text-[#921A40]" />}
+          value="100+"
+          label="Cerita Sukses"
+        />
       </div>
 
       {/* FAQ Section */}
       <div className="md:w-1/2">
-        <p className="text-sm text-gray-600 pb-4">PERTANYAAN PALING SERING DITANYAKAN</p>
-        <h2 className="text-[#921A40] text-4xl font-bold mb-8">Frequently Asked Question</h2>
+        <p className="text-sm text-gray-600 pb-4">
+          PERTANYAAN PALING SERING DITANYAKAN
+        </p>
+        <h2 className="text-[#921A40] text-4xl font-bold mb-8">
+          Frequently Asked Question
+        </h2>
         <div className="space-y-4">
           <FAQItem
             question="Bisakah aku mengikuti Test tanpa mendaftarkan akun?"
@@ -57,7 +84,9 @@ const FAQSection = () => {
 };
 
 const StatCard = ({ icon, value, label, className }) => (
-  <div className={`flex flex-col items-center justify-center p-6 bg-white rounded-lg shadow-md w-48 ${className}`}>
+  <div
+    className={`flex flex-col items-center justify-center p-6 bg-white rounded-lg shadow-md w-48 ${className}`}
+  >
     <div className="p-4 bg-[#FFB1B1] rounded-full">{icon}</div>
     <p className="text-xl mt-2 text-black font-bold">{value}</p>
     <p className="text-sm text-black">{label}</p>
@@ -71,9 +100,11 @@ const FAQItem = ({ question, answer, index, openIndex, toggleFAQ }) => (
       className="flex justify-between items-center w-full text-left  font-semibold"
     >
       <span>{question}</span>
-      <span className="text-[#921A40]">{openIndex === index ? '-' : '+'}</span>
+      <span className="text-[#921A40]">{openIndex === index ? "-" : "+"}</span>
     </button>
-    {openIndex === index && <p className="mt-2 text-sm text-gray-600">{answer}</p>}
+    {openIndex === index && (
+      <p className="mt-2 text-sm text-gray-600">{answer}</p>
+    )}
   </div>
 );
 
