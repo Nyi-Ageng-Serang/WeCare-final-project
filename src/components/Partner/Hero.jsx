46f1fd588a569ebb4eba557e28841cd9a4554a22
Komalasari076 <sitikomala076@gmail.com>
2024-11-08 13:46:41 +0700

    mengganti image di partner page

--- src/components/Partner/Hero.jsx
+++ src/components/Partner/Hero.jsx
@@ -1,10 +1,10 @@
-import partner from "../../assets/pelatihan (2).png";
+import partner from "../../assets/kolaborasi.jpg";
 
 function HeroPartner() {
   return (
     <div className="w-full py-6">
-      <div className="container mx-auto px-10">
-        <div className="flex flex-col lg:flex-row justify-between gap-10 items-center tracking-normal">
+      <div className="container mx-auto px-10 lg:mt-10">
+        <div className="flex flex-col lg:flex-row justify-between gap-20 items-center tracking-normal">
           {/* Partner Text */}
           <div className="w-full">
             <h1 className="lg:text-4xl text-2xl font-bold mb-4 mt-6">
@@ -27,8 +27,8 @@ function HeroPartner() {
           </div>
 
           {/* Partner Image */}
-          <div className="lg:w-3/5 md:w-3/4 h-auto">
-            <img src={partner} alt="Women Career Return" />
+          <div className="lg:w-5/6 md:w-3/4 h-auto ">
+            <img src={partner} alt="Women Career Return" className="rounded-lg" />
           </div>
         </div>
       </div>
